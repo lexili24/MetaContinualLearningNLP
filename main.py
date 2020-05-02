e271f3848fc9e8a06405275ec8947c147b88f36f
Duo Jiang <DuoJiang02@gmail.com>
2020-05-02 13:42:12 -0400

    simplify meta-traning
    separate meta-test and meta-traning
    remove fast-model

--- main.py
+++ main.py
@@ -15,68 +15,70 @@ from task import MetaTask
 import random
 import numpy as np
 
+
 def random_seed(value):
-    torch.backends.cudnn.deterministic=True
+    torch.backends.cudnn.deterministic = True
     torch.manual_seed(value)
     torch.cuda.manual_seed(value)
     np.random.seed(value)
     random.seed(value)
 
-def create_batch_of_tasks(taskset, is_shuffle = True, batch_size = 4):
-    idxs = list(range(0,len(taskset)))
+
+def create_batch_of_tasks(taskset, is_shuffle=True, batch_size=4):
+    idxs = list(range(0, len(taskset)))
     if is_shuffle:
         random.shuffle(idxs)
-    for i in range(0,len(idxs), batch_size):
-        yield [taskset[idxs[i]] for i in range(i, min(i + batch_size,len(taskset)))]
+    for i in range(0, len(idxs), batch_size):
+        yield [taskset[idxs[i]] for i in range(i, min(i + batch_size, len(taskset)))]
 
 def main():
-    
+
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument("--data", default='dataset.json', type=str,
                         help="Path to dataset file")
-    
+
     parser.add_argument("--bert_model", default='bert-base-uncased', type=str,
                         help="Path to bert model")
-    
+
     parser.add_argument("--num_labels", default=2, type=int,
                         help="Number of class for classification")
 
     parser.add_argument("--epoch", default=5, type=int,
                         help="Number of outer interation")
-    
+
     parser.add_argument("--k_spt", default=80, type=int,
                         help="Number of support samples per task")
-    
+
     parser.add_argument("--k_qry", default=20, type=int,
                         help="Number of query samples per task")
 
     parser.add_argument("--outer_batch_size", default=2, type=int,
                         help="Batch of task size")
-    
+
     parser.add_argument("--inner_batch_size", default=12, type=int,
                         help="Training batch size in inner iteration")
-    
+
     parser.add_argument("--outer_update_lr", default=5e-5, type=float,
                         help="Meta learning rate")
-    
+
     parser.add_argument("--inner_update_lr", default=5e-5, type=float,
                         help="Inner update learning rate")
-    
+
     parser.add_argument("--inner_update_step", default=10, type=int,
                         help="Number of interation in the inner loop during train time")
 
     parser.add_argument("--inner_update_step_eval", default=10, type=int,
                         help="Number of interation in the inner loop during test time")
-    
+
     parser.add_argument("--num_task_train", default=500, type=int,
                         help="Total number of meta tasks for training")
-    
+
     parser.add_argument("--num_task_test", default=3, type=int,
                         help="Total number of tasks for testing")
-    
+
     args = parser.parse_args()
-    
+
     reviews = json.load(open(args.data))
     low_resource_domains = ["office_products", "automotive", "computer_&_video_games"]
 
@@ -84,19 +86,19 @@ def main():
     test_examples = [r for r in reviews if r['domain'] in low_resource_domains]
     print(len(train_examples), len(test_examples))
 
-    tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case = True)
+    tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=True)
     learner = Learner(args)
-    
-    test = MetaTask(test_examples, num_task = args.num_task_test, k_support=args.k_spt, 
-                    k_query=args.k_qry, tokenizer = tokenizer)
+
+    test = MetaTask(test_examples, num_task=args.num_task_test, k_support=args.k_spt,
+                    k_query=args.k_qry, tokenizer=tokenizer)
 
     global_step = 0
     for epoch in range(args.epoch):
 
-        train = MetaTask(train_examples, num_task = args.num_task_train, k_support=args.k_spt, 
-                         k_query=args.k_qry, tokenizer = tokenizer)
+        train = MetaTask(train_examples, num_task=args.num_task_train, k_support=args.k_spt,
+                         k_query=args.k_qry, tokenizer=tokenizer)
 
-        db = create_batch_of_tasks(train, is_shuffle = True, batch_size = args.outer_batch_size)
+        db = create_batch_of_tasks(train, is_shuffle=True, batch_size=args.outer_batch_size)
 
         for step, task_batch in enumerate(db):
 
@@ -107,11 +109,11 @@ def main():
             if global_step % 10 == 0:
                 random_seed(123)
                 print("\n-----------------Testing Mode-----------------\n")
-                db_test = create_batch_of_tasks(test, is_shuffle = False, batch_size = 1)
+                db_test = create_batch_of_tasks(test, is_shuffle=False, batch_size=1)
                 acc_all_test = []
 
                 for test_batch in db_test:
-                    acc = learner(test_batch, training = False)
+                    acc = learner(test_batch)
                     acc_all_test.append(acc)
 
                 print('Step:', step, 'Test F1:', np.mean(acc_all_test))
@@ -119,6 +121,7 @@ def main():
                 random_seed(int(time.time() % 10))
 
             global_step += 1
-            
+
+
 if __name__ == "__main__":
     main()
--- maml.py
+++ maml.py
@@ -10,143 +10,122 @@ import torch
 from sklearn.metrics import accuracy_score
 import numpy as np
 
+
 class Learner(nn.Module):
     """
     Meta Learner
     """
+
     def __init__(self, args):
         """
         :param args:
         """
         super(Learner, self).__init__()
-        
+
         self.num_labels = args.num_labels
         self.outer_batch_size = args.outer_batch_size
         self.inner_batch_size = args.inner_batch_size
-        self.outer_update_lr  = args.outer_update_lr
-        self.inner_update_lr  = args.inner_update_lr
+        self.outer_update_lr = args.outer_update_lr
+        self.inner_update_lr = args.inner_update_lr
         self.inner_update_step = args.inner_update_step
         self.inner_update_step_eval = args.inner_update_step_eval
         self.bert_model = args.bert_model
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        
-        self.model = BertForSequenceClassification.from_pretrained(self.bert_model, num_labels = self.num_labels)
+
+        self.model = BertForSequenceClassification.from_pretrained(self.bert_model, num_labels=self.num_labels)
         self.outer_optimizer = Adam(self.model.parameters(), lr=self.outer_update_lr)
         self.model.train()
 
-
-    def forward(self, batch_tasks, training = True):
+    def forward(self, batch_tasks):
         """
         batch = [(support TensorDataset, query TensorDataset),
                  (support TensorDataset, query TensorDataset),
                  (support TensorDataset, query TensorDataset),
                  (support TensorDataset, query TensorDataset)]
-        
+
         # support = TensorDataset(all_input_ids, all_attention_mask, all_segment_ids, all_label_ids)
         """
         task_accs = []
         num_task = len(batch_tasks)
-        num_inner_update_step = self.inner_update_step if training else self.inner_update_step_eval
 
         for task_id, task in enumerate(batch_tasks):
             support = task[0]
-            query   = task[1]
-            wl_params = []
-            fast_model = deepcopy(self.model)
+            query = task[1]
             # Random Initialize W_ for classification
-            torch.nn.init.xavier_uniform_(fast_model.classifier.weight.data)
-            
-            fast_model.to(self.device)
+            torch.nn.init.xavier_uniform_(self.model.classifier.weight.data)
+
+            self.model.to(self.device)
             support_dataloader = DataLoader(support, sampler=RandomSampler(support),
                                             batch_size=self.inner_batch_size)
-            
-            inner_optimizer = Adam(fast_model.parameters(), lr=self.inner_update_lr)
-            
-            fast_model.train()
+
+            inner_optimizer = Adam(self.model.parameters(), lr=self.inner_update_lr)
+
+            self.model.train()
             # Freeze the representation layers of the model to train the inner loop
             # for param in fast_model.bert.embeddings.parameters():
             #     param.requires_grad = False
-            ######## Another method to change the freezing layer
+            # Another method to change the freezing layer
 
-            for name, param in fast_model.named_parameters():
-                if 'classifier' not in name: # classifier layer
+            for name, param in self.model.named_parameters():
+                if 'classifier' not in name:  # classifier layer
                     param.requires_grad = False
                 else:
                     param.requires_grad = True
 
-            
-            print('----Task',task_id, '----')
-            for i in range(0,num_inner_update_step):
-                print ('----Training Inner Step ',i,'-----')
+            print('----Task', task_id, '----')
+            for i in range(0, self.inner_update_step):
+                print('----Training Inner Step ', i, '-----')
                 all_loss = []
                 for inner_step, batch in enumerate(support_dataloader):
-                    
+
                     batch = tuple(t.to(self.device) for t in batch)
                     input_ids, attention_mask, segment_ids, label_id = batch
-                    outputs = fast_model(input_ids, attention_mask, segment_ids, labels = label_id)
-                    
-                    loss = outputs[0]              
+                    outputs = self.model(input_ids, attention_mask, segment_ids, labels=label_id)
+
+                    loss = outputs[0]
                     loss.backward()
                     inner_optimizer.step()
                     inner_optimizer.zero_grad()
-                    
+
                     all_loss.append(loss.item())
-                
-                #if i % 4 == 0:
+
+                # if i % 4 == 0:
                 print("Inner Loss: ", np.mean(all_loss))
 
             # W_l gradient
-            fast_model.to(torch.device('cpu'))
-            for i, params in enumerate(fast_model.parameters()):
-                wl_params.append(deepcopy(params))
+            self.model.to(torch.device('cpu'))
 
             print('S_Test')
             query_dataloader = DataLoader(query, sampler=None, batch_size=len(query))
             query_batch = iter(query_dataloader).next()
             query_batch = tuple(t.to(self.device) for t in query_batch)
             q_input_ids, q_attention_mask, q_segment_ids, q_label_id = query_batch
-            
-            for i, params in enumerate(self.model.parameters()):
-                params = wl_params[i]  
-            q_outputs = self.model(q_input_ids, q_attention_mask, q_segment_ids, labels = q_label_id)
-            
-            if training:
-                for name, param in self.model.named_parameters():
-                    if 'classifier' in name: # classifier layer
-                        param.requires_grad = False
-                    else:
-                        param.requires_grad = True
-                
-                q_loss = q_outputs[0]
-                q_loss.backward()
-                self.outer_optimizer.step()
-                self.outer_optimizer.zero_grad()
 
-            q_logits = F.softmax(q_outputs[1],dim=1)
-            pre_label_id = torch.argmax(q_logits,dim=1)
+            q_outputs = self.model(q_input_ids, q_attention_mask, q_segment_ids, labels=q_label_id)
+
+            for name, param in self.model.named_parameters():
+                if 'classifier' in name:  # classifier layer
+                    param.requires_grad = False
+                else:
+                    param.requires_grad = True
+
+            q_loss = q_outputs[0]
+            q_loss.backward()
+            self.outer_optimizer.step()
+            self.outer_optimizer.zero_grad()
+
+            q_logits = F.softmax(q_outputs[1], dim=1)
+            pre_label_id = torch.argmax(q_logits, dim=1)
             pre_label_id = pre_label_id.detach().cpu().numpy().tolist()
             q_label_id = q_label_id.detach().cpu().numpy().tolist()
-            
-            acc = accuracy_score(pre_label_id,q_label_id)
+
+            acc = accuracy_score(pre_label_id, q_label_id)
             task_accs.append(acc)
             print('Query Acc: ', acc)
-            
-            del fast_model, inner_optimizer
+
+            del inner_optimizer
             torch.cuda.empty_cache()
-        
-        # if training:
-        #     # Average gradient across tasks
-        #     for i in range(0,len(sum_gradients)):
-        #         sum_gradients[i] = sum_gradients[i] / float(num_task)
 
-        #     #Assign gradient for original model, then using optimizer to update its weights
-        #     for i, params in enumerate(self.model.parameters()):
-        #         params.grad = sum_gradients[i]
-
-        #     self.outer_optimizer.step()
-        #     self.outer_optimizer.zero_grad()
-            
-        #     del sum_gradients
             gc.collect()
-        
+
         return np.mean(task_accs)
